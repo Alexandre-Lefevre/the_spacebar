60464a2d2945b2c7d285c8d4b71cf7ffbb193d4a
alexandre_lefevre@outlook.com <45029530+Alexandre-Lefevre@users.noreply.github.com>
2019-04-02 14:56:03 +0200

    premier commit symfony

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,9 +10,10 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -27,9 +28,13 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'blabla de la page : %s',
-            $slug
-        ));
+        $comments =[
+            'Devastation, voyage, and stigma.', 'Where is the senior sun?', 'Technically raise a crewmate.'
+        ];
+
+        return $this->render('article/show.html.twig', [
+                'title' =>ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+            ]);
     }
 }
